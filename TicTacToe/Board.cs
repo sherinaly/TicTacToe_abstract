39739e0e0fbddde7b94eb26e9d3febdbc9ebb804
Sherin Aly <sherin@vt.edu>
2017-01-08 05:13:16 -0500

    first commit

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Class Board that represents the 3x3 playing board.
+    /// </summary>
+    public class Board   
+    { 
+        private char[][] _board;  
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Board" /> class.
+        /// </summary>
+        public Board()
+        {
+            
+        }
+
+        /// <summary>
+        /// Checks if this board cell is empty.
+        /// </summary>
+        /// <param name="cellLocation">The cell location.</param>
+        /// <returns><c>true</c> if cellLocation is empty(of default value), <c>false</c> otherwise.</returns>
+        public bool CheckIfThisBoardCellIsEmpty(Tuple<int, int> cellLocation)
+        {
+            return true;
+        }// return true if (x,y) is empty and false if not
+
+        public Tuple<byte, byte>[] getAllEmptyCellsInBoard()
+        {
+        }
+
+        /// <summary>
+        /// Sets the this symbol at this board cell location. Returns false if the cell is not empty
+        /// </summary>
+        /// <param name="cellLocation">The cell location.</param>
+        /// <param name="symbol">The symbol.</param>
+        /// <returns><c>true</c> if cellLocation is empty, <c>false</c> otherwise.</returns>
+        public bool SetThisSymbolAtThisBoardCellLocation(Tuple<int, int> cellLocation, char symbol)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if any Player won.
+        /// </summary>
+        /// <returns>System.Char.</returns>
+        public char CheckIfAnyPlayerWon()
+        { 
+            return'x';
+        }
+    }
+}
---
+++ TicTacToe/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Player
+    {
+        private bool _iamCurrentlyPlaying;// the playing filed
+        private readonly char _mySymbolOnBoard;  // set by the constructor; could be either ‘x’ or ‘o’
+        private bool _iWon; // initialized to ‘false by constructor
+                               
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Player"/> class.
+        /// </summary>
+        public Player() // Constructor
+        {
+        }
+
+        /// <summary>
+        /// Makes a move. 
+        /// It sets the player symbol on one of the empty cells of the board
+        /// and return a flag either this current player won or not. 
+        /// It calls findEmptyCellLocation to locate the move.
+        /// </summary>
+        public void MakeAMove()
+        {
+
+        }
+        
+
+        /// <summary>
+        /// Finds the empty cell location that can make me win. 
+        /// The selection is based on defined rules here.
+        /// </summary>
+        /// <param name="theBoard">The board.</param>
+        /// <returns>Tuple&lt;System.Int32, System.Int32&gt;.</returns>
+        public Tuple<int, int> FindBestEmptyCellLocation(Board theBoard) 
+        {
+        }
+
+        /// <summary>
+        /// Checks if the current player won.  returns true if player won the game and false o.w.
+        /// </summary>
+        /// <returns><c>true</c> if the player won, <c>false</c> otherwise.</returns>
+        public bool CheckIfIWon(Board theBoard)  
+        {
+        }
+    }
+}
