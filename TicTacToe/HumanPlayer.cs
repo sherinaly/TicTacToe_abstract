ee042b0ef47020b38635eb98ae10f1dfa83d8025
Sherin Aly <sherin@vt.edu>
2017-01-10 05:16:48 -0500

    adding human and computer players

--- TicTacToe/HumanPlayer.cs
+++ TicTacToe/HumanPlayer.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Specialized;
 
 namespace TicTacToe
 {
     class HumanPlayer:Player
     {
+        /// <summary>
+        /// The player name
+        /// </summary>
+        public string name;
         /// <summary>
         /// Makes a random move.
         /// Pick a random empty location on the board
